963dddcd3bd4a690135110ba6a775c7acc67ca88
Shakoorkhana <as4630425@gmail.com>
2023-09-10 07:36:49 +0500

    routes and pages for an app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import SignIn from "./Pages/SignIn/SignIn";
+import SingUp from "./Pages/SignUp/SingUp";
+import Profile from "./Pages/Profile/Profile";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SingUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
