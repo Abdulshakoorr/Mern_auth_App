fd20822921ebae7f049dad29a8840ea87afa56ff
Shakoorkhana <as4630425@gmail.com>
2023-09-10 08:09:33 +0500

    server created for backend

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ import express from "express";
 
 const app = express();
 const PORT = 8000;
+
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Auth App 02",
+    message: "Auth App ",
   });
 });
 
