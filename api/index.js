1ddcbb69789245433f8b62632b3b9d08bd519265
Shakoorkhana <as4630425@gmail.com>
2023-09-10 08:06:16 +0500

    created server for the backend

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Auth App 02",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is listening on PORT http://localhost:${PORT}`);
+});
