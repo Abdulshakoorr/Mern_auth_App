49b2a2c42384004e39a6577c9579dac2920cf87f
Shakoorkhana <as4630425@gmail.com>
2023-09-14 12:38:34 +0500

    completed the functionality of the sign up form FE

--- frontend/src/Pages/SignUp/SingUp.jsx
+++ frontend/src/Pages/SignUp/SingUp.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const SingUp = () => {
   const [registerUser, setRegisterUser] = useState({
@@ -7,21 +9,44 @@ const SingUp = () => {
     email: "",
     password: "",
   });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setRegisterUser({ ...registerUser, [name]: value });
   };
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-    console.log(registerUser);
-    setRegisterUser({
-      username: "",
-      email: "",
-      password: "",
-    });
+
+    try {
+      // console.log(registerUser);
+      setLoading(true);
+      setError(false);
+      const result = await axios.post(
+        "http://localhost:8000/api/v1/all-users",
+        registerUser
+      );
+      toast.success(result?.data.message);
+      console.log(result);
+      setRegisterUser({
+        username: "",
+        email: "",
+        password: "",
+      });
+      setLoading(false);
+      setError(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+      console.log(error.message);
+      toast.error("Couldn't Register User");
+    }
   };
 
+  if (error) {
+    return <p className="text-center mt-16 text-red-600">{error}</p>;
+  }
   return (
     <section>
       <div className="min-h-[91vh] bg-gradient-to-b from-blue-500 to-purple-500 flex justify-center items-center">
@@ -91,7 +116,7 @@ const SingUp = () => {
                 type="submit"
                 className="bg-blue-500 w-full text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
               >
-                Sign Up
+                {loading ? "loading" : "Sign Up"}
               </button>
               <p className="py-4">
                 Already have an account?
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Toaster
+      position="top-right"
+      reverseOrder={false}
+      toastOptions={{
+        success: {
+          duration: 3000,
+          theme: {
+            primary: "green",
+            secondary: "black",
+          },
+        },
+      }}
+    />
+  </React.StrictMode>
+);
